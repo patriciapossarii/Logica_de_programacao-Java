dff8f4acbdb78ba1618696b51b78d8b090304b26
patriciapossarii <patricia.mag.carvalho@gmail.com>
2023-04-14 11:31:34 -0300

    Cadastro de Login

---
+++ Logica/src/CadastroDeLogin.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CadastroDeLogin {
+    public static void main(String[] args) {
+        Scanner nomeInput = new Scanner(System.in);
+        System.out.println("Digite o seu nome: ");
+        String nome = nomeInput.next();
+
+
+        if (nome.equals("") ||
+                nome.isBlank() ||
+                nome.equalsIgnoreCase("admin") ||
+                nome.equalsIgnoreCase("administrador")){
+            System.out.println("Usuário inválido");
+        }else {
+            System.out.println("Cadastrado realizado com sucesso");
+        }
+    }
+
+}
