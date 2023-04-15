7d00be44df050cf2c7571184e0649a116c077e49
patriciapossarii <patricia.mag.carvalho@gmail.com>
2023-04-14 22:04:31 -0300

    Contador

---
+++ Logica/src/academy/devdojo/estruturascondicionais/lacosderepeticao/Aula01Introducao.java
@@ -0,0 +1,11 @@
+package academy.devdojo.estruturascondicionais.lacosderepeticao;
+
+public class Aula01Introducao {
+    public static void main(String[] args) {
+        int contador = 0;
+        while(contador<=10){
+            System.out.println("Contador = "+contador);
+            contador = contador +1;
+        }
+    }
+}
