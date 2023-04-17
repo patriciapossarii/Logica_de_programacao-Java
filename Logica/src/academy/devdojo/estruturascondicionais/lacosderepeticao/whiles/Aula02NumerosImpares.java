9e365f2b495822158bc7768f6a7ce40ffc0da6d2
patriciapossarii <patricia.mag.carvalho@gmail.com>
2023-04-17 20:48:26 -0300

    Numeros Impares

---
+++ Logica/src/academy/devdojo/estruturascondicionais/lacosderepeticao/whiles/Aula02NumerosImpares.java
@@ -0,0 +1,21 @@
+package academy.devdojo.estruturascondicionais.lacosderepeticao.whiles;
+
+import java.util.Scanner;
+
+public class Aula02NumerosImpares {
+
+    public static void main(String[] args) {
+        Scanner numberInput = new Scanner(System.in);
+        System.out.print("Enter with value: ");
+        int number = numberInput.nextInt();
+
+        int i = 0;
+
+        while (i <= number) {
+            if (i % 2 == 1) {
+                System.out.println(i);
+            }
+            i = i + 1;
+        }
+    }
+}
