d13a9433c19384470f684f13e198435605bf92ff
patriciapossarii <patricia.mag.carvalho@gmail.com>
2023-04-19 16:26:54 -0300

    for - Contador

---
+++ Logica/src/academy/devdojo/estruturascondicionais/lacosderepeticao/fors/Aula01IntroducaoFor.java
@@ -0,0 +1,11 @@
+package academy.devdojo.estruturascondicionais.lacosderepeticao.fors;
+
+public class Aula01IntroducaoFor {
+    public static void main(String[] args) {
+        System.out.println("Contador de 0 a 10 : ");
+        for(int i = 0;i<=10;i++){
+            System.out.println(i);
+        }
+
+    }
+}
