155a1fbaa04e172fa9c9ebf63f3ba3e1841aa4d6
patriciapossarii <patricia.mag.carvalho@gmail.com>
2023-04-19 18:50:09 -0300

    array - Introducao

---
+++ Logica/src/academy/devdojo/arrays/Aula01Introdução.java
@@ -0,0 +1,16 @@
+package academy.devdojo.arrays;
+
+public class Aula01Introdução {
+    public static void main(String[] args) {
+        double[] notas = new double [4];
+        notas[0] = 8;
+        notas[1] = 7;
+        notas[2]= 3;
+
+
+
+        System.out.println(notas[2]);
+
+
+    }
+}
