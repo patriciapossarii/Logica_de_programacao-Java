e77dc1e263ac918bcffc1384275e00851a729ec3
patriciapossarii <patricia.mag.carvalho@gmail.com>
2023-04-19 16:36:46 -0300

    for - Quadrado Perfeito

---
+++ Logica/src/academy/devdojo/estruturascondicionais/lacosderepeticao/fors/Aula02QuadradoPerfeito.java
@@ -0,0 +1,11 @@
+package academy.devdojo.estruturascondicionais.lacosderepeticao.fors;
+
+public class Aula02QuadradoPerfeito {
+    public static void main(String[] args) {
+
+        for(int i=15;i<=200;i++){
+            System.out.println(i+"*"+i+" = "+i*i);
+        }
+    }
+
+}
