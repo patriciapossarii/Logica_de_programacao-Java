253518c7d28655a0dc539900a0ca0eb37cd753c0
patriciapossarii <patricia.mag.carvalho@gmail.com>
2023-04-20 16:54:09 -0300

    Multiplicação de Arrays

--- Logica/src/academy/devdojo/arrays/Aula01Introdução.java
+++ Logica/src/academy/devdojo/arrays/Aula01Introdução.java
@@ -5,12 +5,11 @@ public class Aula01Introdução {
         double[] notas = new double [4];
         notas[0] = 8;
         notas[1] = 7;
-        notas[2]= 3;
+        notas[2]= 6.6;
+        notas[3]= 4;
 
-
-
-        System.out.println(notas[2]);
-
-
+        for(int i=0; i<notas.length;i++){
+            System.out.println(notas[i]);
+        }
     }
 }
